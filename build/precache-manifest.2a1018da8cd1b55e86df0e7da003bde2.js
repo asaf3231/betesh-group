92b62ab48a48c394f9135dbf736a65f62f6c6e6a
Asaf Ramati <asaframati@Asafs-Laptop.local>
2024-09-02 20:14:53 +0300

    Resolved dependency conflicts and updated deployment settings

---
+++ build/precache-manifest.2a1018da8cd1b55e86df0e7da003bde2.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e93ec7e13f020f3ef96fb3f8993f47d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd55279e1d227f2ba1ba",
+    "url": "/static/css/2.7f5860a0.chunk.css"
+  },
+  {
+    "revision": "f4317588a5d720331468",
+    "url": "/static/css/main.100c61d4.chunk.css"
+  },
+  {
+    "revision": "fd55279e1d227f2ba1ba",
+    "url": "/static/js/2.0096a939.chunk.js"
+  },
+  {
+    "revision": "d178ecf08716c2a9c9315a26ba2a653b",
+    "url": "/static/js/2.0096a939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "220bb073e041af244e48",
+    "url": "/static/js/3.53e7595a.chunk.js"
+  },
+  {
+    "revision": "f4317588a5d720331468",
+    "url": "/static/js/main.89db45c2.chunk.js"
+  },
+  {
+    "revision": "7bc6538521d7330f568c",
+    "url": "/static/js/runtime-main.b8770200.js"
+  },
+  {
+    "revision": "85de4c77b6f0bbf386b5e267f04cf491",
+    "url": "/static/media/Deluxe_NORMAN_HOTEL_MOCKUP_ROOM_002 - Copy.85de4c77.jpg"
+  },
+  {
+    "revision": "99956ea6122b62eea1e485b7a027ea04",
+    "url": "/static/media/HOTEL-Pool.99956ea6.jpeg"
+  },
+  {
+    "revision": "79e3a543208fae88e3475425a92d5b27",
+    "url": "/static/media/Hotel-Rooms.79e3a543.jpeg"
+  },
+  {
+    "revision": "876f61a7ccc253cb00c8849c44afd9ca",
+    "url": "/static/media/NOR_ATRIUM_001_HR.876f61a7.jpg"
+  },
+  {
+    "revision": "d0a159349caced214be42165fca7a30c",
+    "url": "/static/media/NOR_ATRIUM_005_HR.d0a15934.jpg"
+  },
+  {
+    "revision": "374e14fddf9dd6646ebcf5202e3f7bc8",
+    "url": "/static/media/NOR_CORNER_SUITE_010_HR.374e14fd.jpg"
+  },
+  {
+    "revision": "1ddbcd704c531f3694aeffab0ffdd2f5",
+    "url": "/static/media/NOR_CORNER_SUITE_012_HR.1ddbcd70.jpg"
+  },
+  {
+    "revision": "a0a2a00ea76e2980ac882c4007b8f805",
+    "url": "/static/media/NOR_DELUXE_ROOM_001_HR.a0a2a00e.jpg"
+  },
+  {
+    "revision": "5700ec5837fc2846525307d95d45863b",
+    "url": "/static/media/NOR_DININGS_027_HR.5700ec58.jpg"
+  },
+  {
+    "revision": "3ca1029ee2b60d0a11f92a4843350f58",
+    "url": "/static/media/NOR_GARDEN_SUITE_003_HR.3ca1029e.jpg"
+  },
+  {
+    "revision": "18601c0ff61be592ae6a94e732b72ad8",
+    "url": "/static/media/NOR_GARDEN_SUITE_004_HR.18601c0f.jpg"
+  },
+  {
+    "revision": "4e66912da88c3bfed5ae0d72e189a5d9",
+    "url": "/static/media/NOR_GARDEN_SUITE_005_HR.4e66912d.jpg"
+  },
+  {
+    "revision": "66c8bf040a4e8f13689b14c21631ccc3",
+    "url": "/static/media/NOR_KING_ALBERT_SUITE_004_HR.66c8bf04.jpg"
+  },
+  {
+    "revision": "c3c171b43df24e778adbd191d490e550",
+    "url": "/static/media/NOR_LIBRARY_BAR_003_HR.c3c171b4.jpg"
+  },
+  {
+    "revision": "498cb506ecd8cf4fd72c9a981946470d",
+    "url": "/static/media/NOR_PENTHOUSE_007_HR.498cb506.jpg"
+  },
+  {
+    "revision": "dbe54e3fafd8d239b8e08427821d8a3b",
+    "url": "/static/media/NOR_POOL_014_HR.dbe54e3f.jpg"
+  },
+  {
+    "revision": "4a61ff0bc13536b99cfd68cd01225e25",
+    "url": "/static/media/NOR_POOL_015_HR.4a61ff0b.jpg"
+  },
+  {
+    "revision": "3787733fe454145983b7b0de162adb6b",
+    "url": "/static/media/NOR_POOL_019_HR.3787733f.jpg"
+  },
+  {
+    "revision": "22ad858cac6888fbba5f9c14cfc371bd",
+    "url": "/static/media/NOR_PUBLIC_SPACES_033_HR.22ad858c.jpg"
+  },
+  {
+    "revision": "9a98754f0f3a2201484f1d2694e84722",
+    "url": "/static/media/NOR_RESTAURANT_013_HR.9a98754f.jpg"
+  },
+  {
+    "revision": "ec74d5df613be0545a5e70e0227e35f2",
+    "url": "/static/media/Room-With-A-View-2.ec74d5df.avif"
+  },
+  {
+    "revision": "cbfcc9a362fa356b247b030b5336b2ab",
+    "url": "/static/media/infinity-pool.cbfcc9a3.mp4"
+  },
+  {
+    "revision": "e63cbf9da47c181a998a07dde48b3bd8",
+    "url": "/static/media/pool-and-couch.e63cbf9d.avif"
+  },
+  {
+    "revision": "2bbc96e430acbcadabca2babb794fff9",
+    "url": "/static/media/room-with-a-view.2bbc96e4.avif"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "7e8f90138056726a45f5e8f166301171",
+    "url": "/static/media/tel_aviv_pic.7e8f9013.png"
+  }
+]);
